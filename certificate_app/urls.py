8568562dfeee669cba63581119c978623f4f67df
Niveena KJ <niveenajoseph@gmail.com>
2024-03-14 17:15:46 +0530

    fourth

--- certificate_app/urls.py
+++ certificate_app/urls.py
@@ -1,14 +1,16 @@
-
+ 
 from django.urls import path
 from . import views
 
 
 urlpatterns = [
+    
     path('', views.my_view, name='student_form'),
     path('certificate_show/<int:student_id>/', views.certificate_show, name='certificate_show'),
     path('render-pdf/<int:student_id>/', views.render_pdf_view, name='render_pdf'),
-    path('pdf_view/', views.pdf_view, name='pdf_view'),
+    path('pdf_view/<int:student_id>/', views.pdf_view, name='pdf_view'),
     path('certification_verification/<int:student_id>/', views.certificate_verification, name='certificate_verify'),
+    path('register/',views.registration),
 ]
 
 
--- certificate_app/views.py
+++ certificate_app/views.py
@@ -1,19 +1,31 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-from .models import Student
+from certificate_app.models import Student
 from .forms import MyForm
 from django.shortcuts import render
 from django.template.loader import get_template
 from django.urls import reverse
-from .process import html_to_pdf 
+from django.conf import settings
+import os
+from django.contrib.auth.models import User
 from .models import Student
 from django.template import loader
 from reportlab.pdfgen import canvas
 from reportlab.lib.units import inch
 from reportlab.lib.pagesizes import letter,A4
-from django.conf import settings
-import os
+from reportlab.lib.styles import ParagraphStyle
+from reportlab.platypus import Paragraph
+from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.colors import HexColor
+from reportlab.lib.utils import ImageReader
+import pyqrcode 
+import io
+import png 
+from pyqrcode import QRCode
+import textwrap
+#from .pdf_generator import generate_pdf
 
 
 
@@ -46,92 +58,159 @@ def certificate_show(request, student_id):
 
 
 # display verification page
-def certificate_verification(request, student_id):
+def certificate_verification(request, student_id):     
     student_instance = Student.objects.get(id=student_id)
     context = {'student_instance': student_instance}
     return render(request, 'certificate_verification.html', context)
 
 
-#pdf generation 
-def render_pdf_view( c, student_id):
-    
-    # response = HttpResponse(content_type='application/pdf')
-    # response['Content-Disposition'] = 'attachment; filename="generate.pdf"'
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'inline; filename="generate.pdf"'
-
-    custom_page_size = (620, 440)
-    # Create the PDF object, using the response object as its "file."
-    c = canvas.Canvas(response, bottomup=1,pagesize=custom_page_size)
-    c.translate(inch,inch) #starting point of coordinates
-
-    desired_width = 580
-    desired_height = 400
-
-    # Get all objects from the MyModel model
-    mymodels = Student.objects.all()
-
+def render_pdf_view(request, student_id):
     # Get the specific Student object based on student_id
     student_instance = get_object_or_404(Student, id=student_id)
 
+    # Create a BytesIO buffer to store the PDF content
+    buffer = io.BytesIO()
 
-    for mymodel in mymodels:
-        c.drawImage('pictures\Graduation_Certificate.jpg', -0.7*inch, -0.69*inch, width=desired_width, height=desired_height,mask=None)
-        c.drawString(4.7*inch, 9.1*inch, f"SRC/2024/{student_instance.id}")
+    # Create the response object
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'inline; filename="generate.pdf"'
 
-        c.setFont('Helvetica', 32)
+    custom_page_size = (600, 440)
+    c = canvas.Canvas(buffer, bottomup=1, pagesize=custom_page_size)
+    c.translate(inch, inch)
 
-        # Example name
-        name = mymodel.name
+    desired_width = 596
+    desired_height = 436
 
-        # Calculate the width of the name in points
-        name_width = c.stringWidth(name, 'Helvetica', 12)
-        
-        if len(name) < 10:  # Adjust the threshold as needed
-            # Left-aligned text
-            left_align_x = 2.6 * inch  # Adjust as needed
-            left_align_y = 2.20 * inch  # Adjust as needed
-            c.drawString(left_align_x, left_align_y, name)
-            
-        else:
-            center_align_x = (desired_width - name_width) / 2
-            center_align_y = 2.20 * inch
+    
+    c.drawImage('pictures\GraduationCertificate.jpg', -0.97*inch, -0.97*inch, width=desired_width, height=desired_height, mask=None)
+    font_path = 'static/fonts/Quattrocento-Regular.ttf'
+    pdfmetrics.registerFont(TTFont('Quattrocento', font_path))
+    c.setFont('Quattrocento', 12)  
+    c.drawString(4.5 * inch, 4.5 * inch, f"SRC/2024/{student_instance.id}")
+    
+    # Register Dancing Script font
+    font_path = 'static/fonts/MonteCarlo-Regular.ttf'
+    pdfmetrics.registerFont(TTFont('MonteCarlo', font_path))
 
-            # Center-aligned text
-            c.drawCentredString(center_align_x, center_align_y, name)
-        # center_align_x = (desired_width - name_width) / 2
-        # center_align_y = 2.20 * inch
-         
-        # Center-aligned text
-            c.drawCentredString(center_align_x, center_align_y, name)
+    # Example name
+    name = student_instance.name
 
+    # Set the font for the name
+    c.setFont('MonteCarlo', 40)
+    c.setFillColor(HexColor('#3c3a3f'))
+    # c.setFillColorRGB(0,0,0)
+    # Calculate the width of the name in points
+    name_width = c.stringWidth(name)
 
-        text_object = c.beginText(0.2 * inch, 5.8 * inch)
-        text_object.setFont("Helvetica", 10)
-        line1 = text_object.textLine(f"Student of {mymodel.college_name}, has successfully completed the academic internship")
-        line2 = text_object.textLine(f"program at SinroRobotics Pvt Ltd in {mymodel.course} under the guidance of {mymodel.mentor_name}.")
-        line3 = text_object.textLine(f"The internship spanned from {mymodel.start_date} to {mymodel.end_date}.")
+    
 
-        
-        c.drawText(text_object)
-            
-        c.drawString(4.9*inch, 4.0*inch, str(mymodel.end_date))
-       
-        c.drawString(2.5*inch, 1.0*inch, "verification link")
+    if len(name) < 12:
+        left_align_x = 2.4 * inch
+        left_align_y = 2.20 * inch
+        c.drawString(left_align_x, left_align_y, name)
+    elif len(name)<22:
+        left_align_x = 1.7 * inch
+        left_align_y = 2.20 * inch
+        c.drawString(left_align_x, left_align_y, name)
+    else:
+        center_align_x = (desired_width - name_width) / 2
+        center_align_y = 2.20 * inch
+        c.drawCentredString(center_align_x, center_align_y, name)
 
-    # Close the canvas object    
+    font_path = 'static/fonts/Quattrocento-Regular.ttf'
+    pdfmetrics.registerFont(TTFont('Quattrocento', font_path))
+    my_Style = ParagraphStyle(
+        'My Para style',
+        fontName='Quattrocento',
+        fontSize=12,
+        leading=20,  # Line height
+        charSpace=5.0,  # Character spacing
+        alignment='center',
+        HexColor='#3c3a3f',
+    )
+    
+    # Define your custom style
+    my_Style = getSampleStyleSheet()['BodyText']
+    p1 = Paragraph(f'''Student of <b>{student_instance.college_name}</b>, has successfully completed the academic internship
+        program at SinroRobotics Pvt Ltd in <b>{student_instance.course}</b> under the guidance of <b>{student_instance.mentor_name}</b>. 
+        The internship spanned from <b>{student_instance.start_date}</b> to <b>{student_instance.end_date}</b>.''', my_Style)
+    
+    width = 940
+    height = 400
+    p1.wrapOn(c, 450, 50)
+    p1.drawOn(c, width-930, height-295)
+
+    c.drawImage('pictures\paragraph-end-line.png', 2.45*inch, 0.7*inch, width=100, height=50,mask=None)
+
+    c.setFont('Quattrocento', 12)
+    c.setFillColorRGB(0,0,0)
+    c.drawString(4.7*inch, 0.4*inch, str(student_instance.end_date))
+
+    # Generate QR code
+    base_url = request.build_absolute_uri('/')
+    qr_data = f"{base_url}certificate_verify/{student_instance.id}/"
+    qr = pyqrcode.create(qr_data)
+
+    # Save the QR code as BytesIO
+    qr_buffer = io.BytesIO()
+    qr.png(qr_buffer, scale=6)
+
+    # Save the QR code as a file on the server
+    qr_filename = f"qr_code_{student_instance.id}.png"
+    qr_path = os.path.join(settings.MEDIA_ROOT, qr_filename)
+    qr.png(qr_path, scale=6)
+
+    # Pass the URL or path of the QR code image to the context
+    context = {'qr_code_path': qr_path}
+
+    x = 200
+    y = -10
+    width=50
+    height=50
+    # Draw the QR code image on the PDF
+    qr_image = ImageReader(qr_buffer)
+    c.drawImage(qr_image, x, y, width, height)
+
+    
+
+    
     c.showPage()
     c.save()
+
+    # Rewind the buffer to the beginning
+    buffer.seek(0)
+
+    # Write the buffer content to the response
+    response.write(buffer.read())
+
+    # Close the buffers
+    buffer.close()
+    qr_buffer.close()
 
     return response
 
 
+
 def pdf_view(request, student_id):
+    # Get the student_instance based on student_id
+    student_instance = get_object_or_404(Student, id=student_id)
+
+    # # Generate QR code data
+    # qr_data = f"student_id={student_instance.id}"
+
     # Render the template with the PDF content
     template = get_template('generate.html')
-    context = {'pdf_content': render_pdf_view(request, canvas.Canvas( bottomup=1, pagesize=letter), student_id).content}
+    
+    # Pass both request and qr_data when calling render_pdf_view
+    context = {'pdf_content': render_pdf_view(request, student_instance).content}
     rendered_template = template.render(context)
 
     # Return the rendered template
-    return HttpResponse(rendered_template) 
+    return HttpResponse(rendered_template)
+
+
+def registration(request):
+
+    return render(request,'registration.html')
 
