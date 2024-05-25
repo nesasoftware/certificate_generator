821661eccd80cdb2d6042fb67dfe2100b604a379
Niveena KJ <niveenajoseph@gmail.com>
2024-05-25 16:56:47 +0530

    git ignore

--- certificate_app/views.py
+++ certificate_app/views.py
@@ -1261,7 +1261,7 @@ def render_pdf_view(request, student_id):
 
     # Save the QR code as a file on the server
     qr_filename = f"qr_code_{student_instance.certificate_number}.png"
-    qr_path = os.path.join(settings.MEDIA_ROOT, qr_filename)
+    qr_path = os.path.join(settings.MEDIA_ROOT, f"qrcode/{qr_filename}")
     # print("QR Code Path:", qr_path)
     qr.png(qr_path, scale=6)
 
